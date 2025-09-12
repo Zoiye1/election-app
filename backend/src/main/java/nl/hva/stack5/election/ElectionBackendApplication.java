4b02c2149515fe0fd88e7e340f33bdd12f020818
Michael Hoekema <michael.hoekema@hva.nl>
2025-09-12 13:32:47 +0200

    chore: add backend project

---
+++ backend/src/main/java/nl/hva/stack5/election/ElectionBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.hva.stack5.election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nl/hva/stack5/election/ElectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hva.stack5.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
