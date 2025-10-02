df35069a597aac33e0d668a4bd3973ad5873cb23
Michael Hoekema <michael.hoekema@hva.nl>
2025-10-02 11:58:23 +0200

    feat(auth): initial database configuration and user service

--- backend/src/main/java/nl/hva/stack5/election/ElectionBackendApplication.java
+++ backend/src/main/java/nl/hva/stack5/election/ElectionBackendApplication.java
@@ -3,7 +3,10 @@ package nl.hva.stack5.election;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class ElectionBackendApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/nl/hva/stack5/election/service/UserService.java
@@ -0,0 +1,12 @@
+package nl.hva.stack5.election.service;
+
+import nl.hva.stack5.election.model.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    Optional<User> findById(int id);
+    User createUser(User user);
+    boolean verifyUsernameAndPassword(String username, String password);
+    boolean verifyEmailAndPassword(String email, String password);
+}
