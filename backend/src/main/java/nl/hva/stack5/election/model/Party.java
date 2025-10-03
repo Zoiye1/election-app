8c6a583521a3d98fcde293f58f90349c88758eca
Shivanio Cooman <coomans@hva.nl>
2025-10-03 17:50:16 +0200

    Feat: Made a new class party
    Made a party class party that contains a party name with the amont of votes. still needs some work doesn't work good

---
+++ backend/src/main/java/nl/hva/stack5/election/model/Party.java
@@ -0,0 +1,34 @@
+package nl.hva.stack5.election.model;
+
+import java.util.HashMap;
+
+/**
+ * Party class holds the name of a part and the amount votes a party has
+ */
+
+public class Party {
+
+    private String partyName;
+    private String partyVotes;
+
+    /**
+     *
+     * @param partyName contains the pary name
+     * @param partyVotes contains the pary votes
+     */
+
+    public Party(String partyName, String partyVotes) {
+        this.partyName = partyName;
+        this.partyVotes = partyVotes;
+    }
+
+    // returns the part name
+    public String getPartyName() {
+        return partyName;
+    }
+
+    // returns the partyvotes
+    public String getPartVotes() {
+        return partyVotes;
+    }
+}
