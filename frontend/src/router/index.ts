d7a7bb735019ad2c2f38bdaa2717a1854d6ada74
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-10-08 21:55:04 +0200

    FIX(Index.ts) router changed login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DiscussionView from '@/views/DiscussionView.vue'
 import ElectionDataView from '@/views/ElectionDataView.vue'
+import LoginPage from "@/views/LoginPage.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
