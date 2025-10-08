87d151314059c834784eec0fbed7b14a8ac890b6
Zoiye van Leeuwen <zoiyevanleeuwen@icloud.com>
2025-10-09 01:07:37 +0200

    feat(frontend) added button functionality in discussionview for navigation to create-discussion

--- frontend/src/views/DiscussionView.vue
+++ frontend/src/views/DiscussionView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue'
 import Navbar from '../components/Navbar.vue'
 import DiscussionCard from '../components/DiscussionCard.vue'
+import { useRouter } from 'vue-router'
 
 interface Discussion {
   id: number
@@ -51,6 +52,7 @@ onMounted(() => {
       <div class="flex justify-between items-center mb-8">
         <h1 class="text-3xl font-bold text-gray-800">Discussies</h1>
         <button
+          @click="$router.push('/create-discussion')"
           class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-lg flex items-center gap-2 transition-colors">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
