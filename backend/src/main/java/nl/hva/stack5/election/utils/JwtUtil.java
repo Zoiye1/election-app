f4b5b55181691c6634fcdd274ef9629a802b3c5b
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-10-28 21:42:20 +0100

    Feat(backend(utils(pom.xml))) added new JWT dependecies also added in utils JWTutil

---
+++ backend/src/main/java/nl/hva/stack5/election/utils/JwtUtil.java
@@ -0,0 +1,79 @@
+package nl.hva.stack5.election.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+public class JwtUtil {
+
+    private final String SECRET_KEY = "StemSlimSecretKeyForJWTTokenGenerationAndValidation2025";
+
+    private final long JWT_TOKEN_VALIDITY = 10 * 60 * 60 * 1000; // 10 hours
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // Haal expiration date uit token
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Check of token expired is
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    // Genereer token voor user
+    public String generateToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, email);
+    }
+
+    // Genereer token met extra claims
+    public String generateToken(String email, Map<String, Object> extraClaims) {
+        return createToken(extraClaims, email);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Valideer token
+    public Boolean validateToken(String token, String email) {
+        final String username = extractUsername(token);
+        return (username.equals(email) && !isTokenExpired(token));
+    }
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+}
