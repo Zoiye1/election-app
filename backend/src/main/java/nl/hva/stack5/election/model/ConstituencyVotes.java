76546cb978b48ef30aa3dc651c3bb3b0068c97f6
Shivanio Cooman <dreameyeprod@gmail.com>
2025-10-30 11:00:39 +0100

    feat: made constituencyresults an entity and changed changed Votes to votes

--- backend/src/main/java/nl/hva/stack5/election/model/ConstituencyVotes.java
+++ backend/src/main/java/nl/hva/stack5/election/model/ConstituencyVotes.java
@@ -1,26 +1,45 @@
 package nl.hva.stack5.election.model;
 
+import jakarta.persistence.*;
+
 /**
  * This class holds the name of the constituency and the amount of votes of that constituency
  */
-public class ConstituencyVotes {
-    private String constituencyName;
-    private String Votes;
+    @Entity
+    @Table(name = "constituency_votes")
+    public class ConstituencyVotes {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "constituency_name", nullable = false)
+    private String constituencyName;
+
+    @Column(name = "constituency_votes", nullable = false)
+    private String votes;
+
+    public ConstituencyVotes() {}
     /**
      *
      * @param constituencyName hols the name
-     * @param Votes holds the amount of votes
+     * @param votes holds the amount of votes
      */
-        public ConstituencyVotes(String constituencyName, String Votes) {
+        public ConstituencyVotes(String constituencyName, String votes) {
         this.constituencyName = constituencyName;
-        this.Votes = Votes;
+        this.votes = votes;
     }
 
     public String getConstituencyName() {
         return constituencyName;
     }
+    public void setConstituencyName(String constituencyName) {
+            this.constituencyName = constituencyName;
+    }
     public String getVotes() {
-            return Votes;
+            return votes;
+    }
+    public void setVotes(String Votes) {
+            this.votes = votes;
     }
 }
