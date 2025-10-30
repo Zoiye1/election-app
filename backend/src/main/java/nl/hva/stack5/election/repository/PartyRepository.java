0714d7432d92ccd180e311b21d55ebf07b3d7fb9
Shivanio Cooman <dreameyeprod@gmail.com>
2025-10-30 17:34:20 +0100

    chore: added party repo

---
+++ backend/src/main/java/nl/hva/stack5/election/repository/PartyRepository.java
@@ -0,0 +1,53 @@
+package nl.hva.stack5.election.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import nl.hva.stack5.election.model.Party;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PartyRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     *
+     * @param registeredName holds the registered party name
+     * function checks if the name exists on Party and returns it
+     */
+
+    public Party findByRegisteredName(String registeredName) {
+        try {
+            return entityManager.createQuery("SELECT p FROM Party p WHERE p.registeredName LIKE:registeredName ", Party.class).getSingleResult();
+        }
+        catch (NoResultException e) {
+            return null;
+        }
+
+
+
+    }
+
+    public Party findById(Long id) throws Exception{
+        Party party = entityManager.find(Party.class, id);
+        if (party == null) {
+            throw new Exception("cannot find party with id " + id);
+        }
+        return party;
+    }
+
+    @Transactional
+    public Party save(Party party){
+        if (party.getId() == null) {
+            entityManager.persist(party);
+            return party;
+        }
+        else {
+            return entityManager.merge(party);
+        }
+    }
+
+
+}
