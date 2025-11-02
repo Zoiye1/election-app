f9ee8d6e151c58ff093bedad13ab98928314aae4
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-02 22:17:40 +0100

    feat: convert Candidate to JPA entity

--- backend/src/main/java/nl/hva/stack5/election/model/Candidate.java
+++ backend/src/main/java/nl/hva/stack5/election/model/Candidate.java
@@ -2,6 +2,8 @@ package nl.hva.stack5.election.model;
 
 //TODO: make this class a Entity to be ready to migrate to database.
 
+import jakarta.persistence.*;
+
 /**
  * Candidate represents an individual candidate in an election,
  * storing their personal information and unique identifier.
@@ -9,13 +11,28 @@ package nl.hva.stack5.election.model;
  * @author Matisse Ben Addi
  * @version 1.0
  */
+
+@Entity
+@Table(name = "candidates")
 public class Candidate {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     // Instance variables
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String surname;
+
+    @Column(unique = true, nullable = false)
     private String shortCode;
 
-
+    // Default constructor for JPA
+    public Candidate (){
+    }
 
     /**
      * Constructs a Candidate with the specified personal information and identifier.
