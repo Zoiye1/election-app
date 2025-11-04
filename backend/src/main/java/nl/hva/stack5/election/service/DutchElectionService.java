ddc3a418c534c6afa9279acd15a4456080e4e376
Shivanio Cooman <dreameyeprod@gmail.com>
2025-11-04 09:14:43 +0100

    Chore: Service now registeres the data

--- backend/src/main/java/nl/hva/stack5/election/service/DutchElectionService.java
+++ backend/src/main/java/nl/hva/stack5/election/service/DutchElectionService.java
@@ -1,15 +1,18 @@
 package nl.hva.stack5.election.service;
 
 import nl.hva.stack5.election.model.*;
+import nl.hva.stack5.election.repository.ElectionRepository;
 import nl.hva.stack5.election.utils.PathUtils;
 import nl.hva.stack5.election.utils.xml.*;
 import nl.hva.stack5.election.utils.xml.transformers.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * A demo service for demonstrating how an EML-XML parser can be used inside a backend application.<br/>
@@ -19,8 +22,28 @@ import java.io.IOException;
 @Service
 public class DutchElectionService {
 
+    @Autowired
+    private DutchConstituencyVotesTransformer dutchConstituencyVotesTransformer;
+
+    @Autowired
+    private  ElectionRepository electionRepository;
+
     public Election readResults(String electionId, String folderName) {
         System.out.println("Processing files...");
+
+        // election name holds the given electionId and checks if it exists
+        Election electionName = electionRepository.findById(electionId);
+        if (electionName == null) {
+
+            electionName = new Election(electionId);
+            System.out.println("created election " + electionName );
+            electionName = electionRepository.save(electionName);
+            System.out.println("saved election " + electionName );
+        }
+
+        // sets the given election in the transformer
+        dutchConstituencyVotesTransformer.setElection(electionName);
+
 
         Election election = new Election(electionId);
         DutchElectionParser electionParser = new DutchElectionParser(
@@ -28,7 +51,7 @@ public class DutchElectionService {
                 new DutchCandidateTransformer(election),
                 new DutchResultTransformer(election),
                 new DutchNationalVotesTransformer(election),
-                new DutchConstituencyVotesTransformer(election),
+                dutchConstituencyVotesTransformer,
                 new DutchMunicipalityVotesTransformer(election)
         );
 
@@ -38,9 +61,8 @@ public class DutchElectionService {
             electionParser.parseResults(electionId, PathUtils.getResourcePath("/%s".formatted(folderName)));
             // Do what ever you like to do
             System.out.println("Dutch Election results: " + election);
-            // Now is also the time to send the election information to a database for example.
-
-            return election;
+            // Returns the election
+            return electionRepository.findById(electionId);
         } catch (IOException | XMLStreamException | NullPointerException | ParserConfigurationException | SAXException e) {
             // FIXME You should do here some proper error handling! The code below is NOT how you handle errors properly!
             System.err.println("Failed to process the election results!");
