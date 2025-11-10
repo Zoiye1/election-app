233bd6eedc6207aaca611723fecd1a768eecf923
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-10 21:47:58 +0100

    feat: add CandidateController with endpoint to get all candidates

---
+++ backend/src/main/java/nl/hva/stack5/election/controller/CandidateController.java
@@ -0,0 +1,27 @@
+package nl.hva.stack5.election.controller;
+
+import nl.hva.stack5.election.model.Candidate;
+import nl.hva.stack5.election.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "candidates")
+@CrossOrigin(origins = "http://localhost:5173")
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @GetMapping
+    public List<Candidate> getAllCandidates() {
+        return candidateService.getAllCandidates();
+    }
+
+}
+
