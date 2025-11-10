11cede0325d85e8cde652cbde560770ddf487f3c
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-10 21:12:07 +0100

    feat: make getCandidateById and getCandidateByShortcode functions in candidateServiceImpl

--- backend/src/main/java/nl/hva/stack5/election/service/CandidateService.java
+++ backend/src/main/java/nl/hva/stack5/election/service/CandidateService.java
@@ -2,9 +2,13 @@ package nl.hva.stack5.election.service;
 
 import nl.hva.stack5.election.model.Candidate;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CandidateService {
 
-    // Create
-    Candidate createCandidate(Candidate candidate);
-    Candidate createMultipleCandidate(Candidate candidate);
+    // Read
+    Optional<Candidate> getCandidateById(int candidateId);
+    Optional<Candidate> getCandidateByShortCode(String shortCode);
+    List<Candidate> getAllCandidates();
 }
---
+++ backend/src/main/java/nl/hva/stack5/election/service/CandidateServiceImpl.java
@@ -0,0 +1,35 @@
+package nl.hva.stack5.election.service;
+
+
+import nl.hva.stack5.election.model.Candidate;
+import nl.hva.stack5.election.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CandidateServiceImpl implements CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Override
+    public Optional<Candidate> getCandidateById(int candidateId) {
+        return candidateRepository.findById(candidateId);
+    }
+
+    @Override
+    public Optional<Candidate> getCandidateByShortCode(String shortCode) {
+        return candidateRepository.findByShortCode(shortCode);
+    }
+
+    @Override
+    public List<Candidate> getAllCandidates() {
+        return List.of();
+    }
+
+    //Read
+
+}
