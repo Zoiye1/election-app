bc42f56d250c92120e8a2a0b265d0696d8e3abdd
Shivanio Cooman <dreameyeprod@gmail.com>
2025-11-13 09:42:37 +0100

    refacor: addition of a new endpoint get election
    this endpoint will get the entire election based on a year

--- frontend/src/services/ElectionService.ts
+++ frontend/src/services/ElectionService.ts
@@ -1,4 +1,5 @@
 import type { ConstituencyPartyVotes } from "@/interfaces/IElectionData";
+import type { Election } from "@/interfaces/IElectionData";
 
 /**
  *
@@ -11,21 +12,20 @@ import type { ConstituencyPartyVotes } from "@/interfaces/IElectionData";
    * @param constituencyName holds the name of the selected constituency
    * @param folder holds the folder name of where the xml file is located
    */
-  public async getElectionData(electionId: string, constituencyName: string): Promise<ConstituencyPartyVotes[]> {
+  public async getConstituencyData(electionId: string, constituencyName: string): Promise<ConstituencyPartyVotes[]> {
     const url: string = `http://localhost:8080/elections/${electionId}/${constituencyName}`
     // Response holds the fetch to the endpoint
     try {
       // Response holds the fetch to the endpoint
       const response: Response = await fetch(url, {
         method: "GET",
-        headers:{"Accept": "application/json"}
+        headers: {"Accept": "application/json"}
       })
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error("request failed" + response.statusText);
       }
       return await response.json();
-    }
-    catch(error) {
+    } catch (error) {
       console.error('Fetch error:', error);
       throw error;
     }
@@ -38,16 +38,35 @@ import type { ConstituencyPartyVotes } from "@/interfaces/IElectionData";
     try {
       const response: Response = await fetch(url, {
         method: "GET",
-        headers:{"Accept": "application/json"}
+        headers: {"Accept": "application/json"}
       })
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error("request failed" + response.statusText);
       }
       return await response.json();
-    }
-    catch(error) {
+    } catch (error) {
       console.error('Fetch error:', error);
       throw error;
     }
+  }
+
+  public async getElection(electionId: string): Promise<Election> {
+    const url: string = `http://localhost:8080/elections/${electionId}`
+    // Response holds the fetch to the endpoint
+    try {
+      // Response holds the fetch to the endpoint
+      const response: Response = await fetch(url, {
+        method: "GET",
+        headers: {"Accept": "application/json"}
+      })
+      if (!response.ok) {
+        throw new Error("request failed" + response.statusText);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    }
+
   }
 }
