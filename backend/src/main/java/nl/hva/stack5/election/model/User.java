d5a5c7210870bca918a8f31869b061ebf23ee5b5
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-11-20 23:11:51 +0100

    feat(model): add verification column to user entity

--- backend/src/main/java/nl/hva/stack5/election/model/User.java
+++ backend/src/main/java/nl/hva/stack5/election/model/User.java
@@ -19,12 +19,19 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "verification_code", length = 64)
+    private String verificationCode;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
     public User() {}
 
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.enabled = false;
     }
 
     public Integer getId() {
@@ -57,5 +64,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 }
