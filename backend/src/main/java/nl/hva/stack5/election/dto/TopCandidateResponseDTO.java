155c94fadce4a5285af3f0d30aee682a1a142796
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 18:22:04 +0100

    Docs: add JSDock to TopCandidateResponseDTO

--- backend/src/main/java/nl/hva/stack5/election/dto/TopCandidateResponseDTO.java
+++ backend/src/main/java/nl/hva/stack5/election/dto/TopCandidateResponseDTO.java
@@ -1,5 +1,13 @@
 package nl.hva.stack5.election.dto;
 
+
+/**
+ * Data Transfer Object for top candidate information.
+ * Contains candidate details with their election results including votes and party affiliation.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
 public class TopCandidateResponseDTO {
     private long id;
     private String fullName;
