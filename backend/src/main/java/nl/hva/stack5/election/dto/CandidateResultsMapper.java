ebc15eadfc54812eb3ebc0d028fa0fc580210230
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 18:34:51 +0100

    feat: add CandidateResult Mapper with imports

---
+++ backend/src/main/java/nl/hva/stack5/election/dto/CandidateResultsMapper.java
@@ -0,0 +1,8 @@
+package nl.hva.stack5.election.dto;
+
+import nl.hva.stack5.election.dto.TopCandidateResponseDTO;
+import nl.hva.stack5.election.model.Candidate;
+import nl.hva.stack5.election.model.CandidateResult;
+
+public class CandidateResultsMapper {
+}
