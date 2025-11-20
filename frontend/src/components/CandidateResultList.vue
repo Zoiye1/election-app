a1cf4f3b88503b6b7a63b2b0fad3bf9f88b6728e
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 22:34:14 +0100

    feat: add props and state to component

---
+++ frontend/src/components/CandidateResultList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { getTopCandidatesByElection } from '@/services/candidateResultService';
+import type { CandidateResult } from '@/interfaces/candidateResult';
+
+//props
+const props = defineProps<{
+  electionId: string;
+}>();
+
+// State
+const candidateResults = ref<CandidateResult[]>([]);
+const loading = ref<boolean>(true);
+const error = ref<string | null>(null);
