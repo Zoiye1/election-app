e278fd7ed672ac78d7fd7841caeea62306b6561d
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 22:02:37 +0100

    FIX: change import typo

--- frontend/src/services/CandidateResultService.ts
+++ frontend/src/services/CandidateResultService.ts
@@ -1,4 +1,4 @@
-import type { CandidateResult } from '@/interfaces/topCandidate';
+import type { CandidateResult } from '@/interfaces/CandidateResult';
 
 const API_BASE_URL = 'http://localhost:8080/api';
 
