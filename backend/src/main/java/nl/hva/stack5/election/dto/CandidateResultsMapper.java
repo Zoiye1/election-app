28e4ac8e82c618785ea7ec205fae9222372369b1
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 18:35:53 +0100

    feat: add JSDock to top of CandidateResultsMapper

--- backend/src/main/java/nl/hva/stack5/election/dto/CandidateResultsMapper.java
+++ backend/src/main/java/nl/hva/stack5/election/dto/CandidateResultsMapper.java
@@ -4,5 +4,13 @@ import nl.hva.stack5.election.dto.TopCandidateResponseDTO;
 import nl.hva.stack5.election.model.Candidate;
 import nl.hva.stack5.election.model.CandidateResult;
 
+
+/**
+ * Mapper for converting between CandidateResult entity and TopCandidateResponseDTO.
+ * Provides static methods for entity-to-DTO conversion.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
 public class CandidateResultsMapper {
 }
