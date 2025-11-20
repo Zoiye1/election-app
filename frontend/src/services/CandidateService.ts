ccb9dc6007a156cbc258088c4c994c6c7160f0f5
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 17:24:03 +0100

    feat: parse JSON response in getAllCandidates

--- frontend/src/services/CandidateService.ts
+++ frontend/src/services/CandidateService.ts
@@ -13,4 +13,7 @@ export async function getAllCandidates (): Promise<Candidate[]>{
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
+
+  const data: Candidate[] = await response.json();
+  return data;
 }
