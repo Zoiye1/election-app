8d1306dd3fa473631fa82921a8bbd42c64b88354
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 14:23:27 +0100

    feat: add constructor to CandidateResponseDTO

---
+++ backend/src/main/java/nl/hva/stack5/election/dto/CandidateResponseDTO.java
@@ -0,0 +1,21 @@
+package nl.hva.stack5.election.dto;
+
+/**
+ * Data Transfer Object for Candidate display information.
+ * Contains only user-relevant candidate data.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
+public class CandidateResponseDTO {
+    private Long candidateId;
+    private String candidateName;
+    private String candidateSurname;
+
+    //constructor with fields
+    public CandidateResponseDTO (Long candidateId, String candidateName, String candidateSurname) {
+        this.candidateId = candidateId;
+        this.candidateName = candidateName;
+        this.candidateSurname = candidateSurname;
+    }
+}
