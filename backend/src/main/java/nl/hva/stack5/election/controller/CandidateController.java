402a3f061ff951e547d4f5f1bf6a80945e8f1c51
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 16:48:15 +0100

    feat: add /api base path to application.properties

--- backend/src/main/java/nl/hva/stack5/election/controller/CandidateController.java
+++ backend/src/main/java/nl/hva/stack5/election/controller/CandidateController.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @version 1.0
  */
 @RestController
-@RequestMapping(value = "candidates")
+@RequestMapping(value = "/candidates")
 @CrossOrigin(origins = "http://localhost:5173")
 public class CandidateController {
 
