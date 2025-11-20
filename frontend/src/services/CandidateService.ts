15ddc71d200478b2dd620dca74df086a8e3eec55
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-20 17:18:42 +0100

    feat: creat getAllCandidates function in frontend service

---
+++ frontend/src/services/CandidateService.ts
@@ -0,0 +1,12 @@
+import type { Candidate } from "@/interfaces/Candidate.ts";
+
+const API_BASE_URL = 'http://localhost:8080/api';
+
+
+/**
+ * Fetches all candidates from the backend
+ * @returns {Promise<Candidate[]>} List of all candidates
+ */
+export async function getAllCandidates (): Promise<Candidate[]>{
+
+}
