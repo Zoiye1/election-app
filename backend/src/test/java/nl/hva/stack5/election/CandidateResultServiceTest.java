b4efc1072dacc7a50043a465a4d9f604a3dd6d00
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-21 15:19:46 +0100

    test: add helper method for mockresults

--- backend/src/test/java/nl/hva/stack5/election/CandidateResultServiceTest.java
+++ backend/src/test/java/nl/hva/stack5/election/CandidateResultServiceTest.java
@@ -1,12 +1,18 @@
 package nl.hva.stack5.election;
 
 
+import nl.hva.stack5.election.model.Candidate;
+import nl.hva.stack5.election.model.CandidateResult;
+import nl.hva.stack5.election.model.Election;
+import nl.hva.stack5.election.model.Party;
 import nl.hva.stack5.election.repository.CandidateRepository;
 import nl.hva.stack5.election.repository.CandidateResultRepository;
 import nl.hva.stack5.election.service.CandidateResultService;
 import nl.hva.stack5.election.service.CandidateResultServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
@@ -38,9 +44,29 @@ public class CandidateResultServiceTest {
     @Test
     void getTopCandidatesByElection_shouldReturnTopCandidates_whenResultsExist() {
         //Arrange
-
+        String electionId = "TK2023";
+        int limit = 10;
+        //Mock repo results
+        List<CandidateResult> mockResults = createMockCandidateResults();
         //Act
 
         //Assert
+    }
+
+    private List<CandidateResult> createMockCandidateResults() {
+
+        Candidate candidate1 = new Candidate("Geert", "Wilders", "WildersG");
+
+        Party party1 = new Party("PVV");
+
+        Election election1 = new Election("TK2023");
+
+
+        //makes candidate result out of mock data above
+        CandidateResult result1 = new CandidateResult(election1, party1, candidate1, "500000");
+
+
+        //returns a list of the candidateresult we made above
+        return List.of(result1);
     }
 }
