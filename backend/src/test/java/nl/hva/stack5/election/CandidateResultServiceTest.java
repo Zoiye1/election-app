440f45b9a8708e934ade717bd0d925741d3d1782
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-21 14:32:49 +0100

    test: add CandidateResultServiceTest

---
+++ backend/src/test/java/nl/hva/stack5/election/CandidateResultServiceTest.java
@@ -0,0 +1,12 @@
+package nl.hva.stack5.election;
+
+
+/**
+ * Unit tests for CandidateResultService.
+ * Tests business logic in isolation using Mockito mocks.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
+public class CandidateResultServiceTest {
+}
