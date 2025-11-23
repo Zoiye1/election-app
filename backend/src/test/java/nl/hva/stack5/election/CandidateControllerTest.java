5fd694c1a8373c43072729db1c60ad0abcad266b
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-23 19:36:47 +0100

    test: add testfile for candidateController

---
+++ backend/src/test/java/nl/hva/stack5/election/CandidateControllerTest.java
@@ -0,0 +1,11 @@
+package nl.hva.stack5.election;
+
+/**
+ * Unit tests for CandidateController.
+ * Tests REST API endpoints in isolation using MockMvc.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
+public class CandidateControllerTest {
+}
