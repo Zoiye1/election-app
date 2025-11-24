a338d409273608e9bf94b2ae962c321537e29847
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-24 15:15:10 +0100

    feat: add available ellections const

--- frontend/src/composables/useElection.ts
+++ frontend/src/composables/useElection.ts
@@ -1,3 +1,11 @@
 import { ref } from 'vue'
 
+//standard "TK2025"
 const selectedElection = ref<string>('TK2025')
+
+// elections that can be chosen from in selector
+const availableElections = [
+  { year: '2021', id: 'TK2021'},
+  { year: '2023', id: 'TK2023'},
+  { year: '2025', id: 'TK2025'},
+]
