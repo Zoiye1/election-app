783eed5d8b65103c9f02cc7d7037a6a65907c47b
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-24 15:43:07 +0100

    feat: add template with loop for each election id and active true or false conditioning

--- frontend/src/components/ElectionSelectorComponent.vue
+++ frontend/src/components/ElectionSelectorComponent.vue
@@ -1,3 +1,21 @@
 <script setup lang="ts">
+import { useElection} from '@/composables/useElection.ts'
 
+// retrieve shared state and function
+const { selectedElection, availableElections, setElection } = useElection()
 </script>
+
+
+<template>
+<div class="election-selector">
+  <button
+    v-for="election in availableElections"
+    :key="election.id"
+    @click="setElection(election.id)"
+    :class="{ active: election.id === selectedElection.id }"
+    class="election-btn"
+    >
+    {{election.year}}
+  </button>
+</div>
+</template>
