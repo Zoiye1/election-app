07cb8e327e99e5b433f6aec021955ddcd069c246
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-27 01:09:00 +0100

    feat: add props and states to component

---
+++ frontend/src/components/PartyResultList.vue
@@ -0,0 +1,23 @@
+
+<script setup lang="ts">
+
+
+// Props
+import type { TopNationalParty } from '@/interfaces/IElectionData.ts'
+import { ref } from 'vue'
+
+const props = defineProps<{
+  electionId: string
+}>()
+
+
+//TODO: create composables for shared states
+// State
+const partyResults = ref<TopNationalParty[]>([])
+const loading = ref<boolean>(true)
+const error = ref<string | null>(null)
+</script>
+
+
+<template>
+</template>
