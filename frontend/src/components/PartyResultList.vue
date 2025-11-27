b573db0da8d8560d44a7ffaa4f08a4c33b02044d
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-27 01:20:25 +0100

    feat: create header in TopParties template

--- frontend/src/components/PartyResultList.vue
+++ frontend/src/components/PartyResultList.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-
-
 // Props
 import type { TopNationalParty } from '@/interfaces/IElectionData.ts'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { NationalPartyService } from '@/services/NationalPartyService.ts'
 
 const props = defineProps<{
   electionId: string
 }>()
-
 
 //TODO: create composables for shared states
 // State
@@ -24,14 +21,37 @@ async function fetchPartyResults() {
   try {
     // Fetch data from API
     partyResults.value = await NationalPartyService.getTopParties(props.electionId)
-    } catch (err) {
+  } catch (err) {
     // If it does not succeed display error
     error.value = 'Failed to fetch party results'
     console.error(err)
-    } finally{
+  } finally {
     loading.value = false
   }
 }
+
+// watches prop changes, if change is detected refetch data fro correct election.
+watch(
+  () => props.electionId,
+  () => {
+    fetchPartyResults()
+  },
+  { immediate: true },
+)
 </script>
 
-<template></template>
+<template>
+  <div class="max-w-4xl mx-auto p-6 bg-white rounded-xl shadow-md">
+    <template>
+      <div class="max-w-4xl mx-auto p-6 bg-white rounded-xl shadow-md">
+        <div class="flex items-center justify-between mb-6">
+          <h2 class="text-2xl font-bold text-gray-800 flex items-center gap-2">
+            📈 Top Partijen Nationaal
+          </h2>
+        </div>
+      </div>
+    </template>
+
+  </div>
+</template>
+
