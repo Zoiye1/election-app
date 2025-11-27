51b6ebce2d6ca62fce061583302e5d56f50db389
Matisse Ben Addi <matissegreetz@gmail.com>
2025-11-27 01:14:23 +0100

    feat: create fetchPartyResiults Function

--- frontend/src/components/PartyResultList.vue
+++ frontend/src/components/PartyResultList.vue
@@ -1,10 +1,10 @@
-
 <script setup lang="ts">
 
 
 // Props
 import type { TopNationalParty } from '@/interfaces/IElectionData.ts'
 import { ref } from 'vue'
+import { NationalPartyService } from '@/services/NationalPartyService.ts'
 
 const props = defineProps<{
   electionId: string
@@ -16,8 +16,22 @@ const props = defineProps<{
 const partyResults = ref<TopNationalParty[]>([])
 const loading = ref<boolean>(true)
 const error = ref<string | null>(null)
+
+async function fetchPartyResults() {
+  loading.value = true
+  error.value = null
+
+  try {
+    // Fetch data from API
+    partyResults.value = await NationalPartyService.getTopParties(props.electionId)
+    } catch (err) {
+    // If it does not succeed display error
+    error.value = 'Failed to fetch party results'
+    console.error(err)
+    } finally{
+    loading.value = false
+  }
+}
 </script>
 
-
-<template>
-</template>
+<template></template>
