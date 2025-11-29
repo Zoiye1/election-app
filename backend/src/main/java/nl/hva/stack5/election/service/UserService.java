7703d612520685f7dc6d1b60e3cd1cb1e1bf8a23
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-11-29 20:54:56 +0100

    Refoctor(UserServiceinterface) update the code

--- backend/src/main/java/nl/hva/stack5/election/service/UserService.java
+++ backend/src/main/java/nl/hva/stack5/election/service/UserService.java
@@ -9,4 +9,6 @@ public interface UserService {
     User createUser(User user);
     boolean verifyUsernameAndPassword(String username, String password);
     boolean verifyEmailAndPassword(String email, String password);
+    User updateUsername(Integer userId, String newUsername);
+    void deleteUser(Integer userId);
 }
