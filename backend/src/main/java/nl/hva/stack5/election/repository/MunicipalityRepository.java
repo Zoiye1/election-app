2259a256f9c366a255dae415110cb0630a6e83a8
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-11-29 18:33:00 +0100

    feat(repositroy) add municipality repo

---
+++ backend/src/main/java/nl/hva/stack5/election/repository/MunicipalityRepository.java
@@ -0,0 +1,30 @@
+package nl.hva.stack5.election.repository;
+import jakarta.persistence.EntityManager;
+import nl.hva.stack5.election.dto.MunicipalityPartyVotesDTO;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class MunicipalityRepository {
+    private EntityManager entityManager;
+
+    public List<MunicipalityPartyVotesDTO> findByMunicipalityAndElectionId(
+            String electionId, String municipalityName) {
+
+        return entityManager.createQuery(
+                        "SELECT new nl.hva.stack5.election.dto.MunicipalityPartyVotesDTO(" +
+                                "p.registeredName, m.name, SUM(v.votes)) " +
+                                "FROM MunicipalityPartyVotes v " +
+                                "JOIN v.party p " +
+                                "JOIN v.municipality m " +
+                                "WHERE v.election.id = :electionId " +
+                                "AND m.name = :municipalityName " +
+                                "GROUP BY p.registeredName, m.name " +
+                                "ORDER BY SUM(v.votes) DESC",
+                        MunicipalityPartyVotesDTO.class)
+                .setParameter("electionId", electionId)
+                .setParameter("municipalityName", municipalityName)
+                .getResultList();
+    }
+
+}
