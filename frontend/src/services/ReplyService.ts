da18541aebb826613dc7b053e651a4edca384851
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-10 15:02:23 +0100

    feat: add createreply function in ReplyService frontend

--- frontend/src/services/ReplyService.ts
+++ frontend/src/services/ReplyService.ts
@@ -2,4 +2,20 @@ import type { CreateReplyRequest, ReplyResponseDTO } from "@/interfaces/Replies"
 
 export class ReplyService {
   private baseUrl: string = 'http://localhost:8080/api/v1/replies'
+
+  public async createReply(data: CreateReplyRequest): Promise <ReplyResponseDTO>{
+
+    const response: Response = await fetch(this.baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        },
+      body: JSON.stringify(data)
+    })
+
+    if (!response.ok) {
+      throw new Error('request failed: ' + response.statusText)
+    }
+  }
 }
