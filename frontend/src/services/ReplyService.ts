e4bb42c885f84eac71aebd2bb47dddd4e7063966
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-10 14:43:44 +0100

    feat: create ReplyService and add instance of base url

---
+++ frontend/src/services/ReplyService.ts
@@ -0,0 +1,5 @@
+import type { CreateReplyRequest, ReplyResponseDTO } from "@/interfaces/Replies";
+
+export class ReplyService {
+  private baseUrl: string = 'http://localhost:8080/api/v1/replies'
+}
