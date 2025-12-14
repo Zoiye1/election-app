c1b3b81f087975c7943c4d76cbe327bc03072db3
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2025-12-14 17:42:46 +0100

    Feat(DTO) add user response DTO

---
+++ backend/src/main/java/nl/hva/stack5/election/dto/UserResponseDTO.java
@@ -0,0 +1,40 @@
+package nl.hva.stack5.election.dto;
+
+public class UserResponseDTO {
+
+    private Integer id;
+    private String username;
+    private String email;
+
+    public UserResponseDTO() {}
+
+    public UserResponseDTO(Integer id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
