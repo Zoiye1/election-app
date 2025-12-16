33b7f2e22cc969344d11e5bb6fa20f5cf5833ab9
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-16 12:58:43 +0100

    feat: create HandleResponseStatusExc3ption method for GBExceptionHandler

--- backend/src/main/java/nl/hva/stack5/election/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/nl/hva/stack5/election/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +21,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
         ErrorResponse error = new ErrorResponse(500, ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    /**
+     * Handles ResponseStatusExceptions
+     *
+     * @param ex The ResponseStatusException that was thrown
+     * @return ResponseEntity with corresponding status code and error message
+     */
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex) {
+        ErrorResponse error = new ErrorResponse(ex.getStatusCode().value(), ex.getMessage());
+        return ResponseEntity.status(ex.getStatusCode()).body(error);
     }
 }
