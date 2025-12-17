99b84c99277aeb3f3ee68c90f21e0562989077a0
Shivanio Cooman <dreameyeprod@gmail.com>
2025-12-17 16:09:23 +0100

    feat: partyInformationComponent now displays top performing consttiuencies of a selected party

--- frontend/src/components/ConstituencyComponents/PartyInformationComponent.vue
+++ frontend/src/components/ConstituencyComponents/PartyInformationComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, watch } from "vue";
-import { ConstituencyService } from "@/services/ConstituencyService";
+import { ConstituencyService } from "@/services/ConstituencyService"
+import type { TopConstituencies } from "@/interfaces/IElectionData"
 
 const props = defineProps<{
   name: string;
@@ -13,35 +14,46 @@ const error = ref<boolean>(false)
 const errorMessage = `Er is geen data beschikbaar voor deze partij in dit jaar 😬 `
 const emptyMessage = "Selecteer een partij";
 
+
 const percentage = ref<number>(0);
-onMounted(async () => {
+const topConstituencies = ref<TopConstituencies[]>([])
+
+onMounted(async ()=>{
+
   try {
-    const constituencyVotesPercentage = await ConstituencyService.getConstituencyVotesPercentage(props.election, props.name, props.party);
-    percentage.value = constituencyVotesPercentage;
-
+    percentage.value = await ConstituencyService.getConstituencyVotesPercentage(props.election, props.name, props.party)
   }
-  catch (e) {
-
+  catch (e){
     percentage.value = 0;
 
   }
-
-});
-
-watch(props, async (newValue) => {
-
   try {
-    const constituencyVotesPercentage = await ConstituencyService.getConstituencyVotesPercentage(newValue.election, newValue.name, newValue.party);
-    percentage.value = constituencyVotesPercentage;
+    topConstituencies.value = await ConstituencyService.getTopPerformingConstituencyByParty(props.election, props.party)
+  }
+  catch (e){
+    topConstituencies.value = []
+
+  }
+})
+
+watch(props, async (newValue) =>{
+  try {
+    percentage.value = await ConstituencyService.getConstituencyVotesPercentage(newValue.election, newValue.name, newValue.party)
     error.value = false;
   }
-  catch (e) {
+  catch (e){
+    percentage.value = 0;
+    error.value = true;
 
-   error.value = true;
   }
+  try {
+    topConstituencies.value = await ConstituencyService.getTopPerformingConstituencyByParty(newValue.election, newValue.party)
+  }
+  catch (e){
+    topConstituencies.value = []
 
-});
-
+  }
+})
 const formattedPercentage = computed(() => {
   if (percentage.value < 0.1) {
     return "minder dan 0,1%";
@@ -72,18 +84,27 @@ const formattedPercentage = computed(() => {
     <div v-else-if="error" class="bg-red-50 text-red-600 border border-red-200 rounded-lg px-4 py-3 mb-4">
       {{ errorMessage }}
     </div>
-    <div v-else class="bg-white shadow-md p-6 rounded-xl border border-purple-100">
-      <p class="text-gray-800 text-sm mb-2">Aandeel binnen de kieskring</p>
+    <div v-else>
+      <div class="bg-white shadow-md p-6 rounded-xl border border-purple-100 mb-5">
+        <p class="text-gray-800 text-sm mb-2">Aandeel binnen de kieskring</p>
 
-      <p class="text-gray-700 text-lg font-bold">
-        Deze partij behaalde
-        <span class="text-purple-600 font-bold">{{formattedPercentage}}</span>
-        van alle uitgebrachte stemmen in deze kieskring.
-      </p>
+        <p class="text-gray-700 text-lg font-bold">
+          Deze partij behaalde
+          <span class="text-purple-600 font-bold">{{formattedPercentage}}</span>
+          van alle uitgebrachte stemmen in deze kieskring.
+        </p>
+      </div>
 
+
+      <div class="bg-white shadow-md p-6 rounded-xl border border-purple-100">
+        <p class="text-gray-800 text-sm mb-2">Top 5 populairste kieskringen</p>
+        <ul>
+          <li v-for="(c, i) in topConstituencies" :key="c.constituencyName">
+            <span class= "text-gray-700 text-lg font-bold"> {{i + 1}}. {{ c.constituencyName }}</span> | <span class="text-purple-600 font-bold">{{ c.votes.toLocaleString() }} stemmen</span>
+          </li>
+        </ul>
+      </div>
     </div>
-
-
 
   </div>
 </template>
