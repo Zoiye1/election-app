c32272d3d5d20bf6c6a4620d1bad845b39d28aef
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-20 00:15:11 +0100

    feat: create CandidateDetailModal file

---
+++ frontend/src/components/CandidateDetailModal.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { CandidateResult } from '@/interfaces/CandidateResult'
+
+const props = defineProps<{
+  candidate: CandidateResult
+  ranking: number
+}>()
+
+
+</script>
+
+<template>
+
+</template>
