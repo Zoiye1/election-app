fa634e32caca8d7a055ba4f6132781539efeb959
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-19 12:52:26 +0100

    fix: clean up electionService of duplicate constituency methods

--- frontend/src/services/ElectionService.ts
+++ frontend/src/services/ElectionService.ts
@@ -10,24 +10,6 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export class ElectionService {
 
-  public async getAllMunicipalityData(electionId: string): Promise<any[]> {
-    const url = `${API_BASE_URL}/elections/${electionId}/municipalities`;
-
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {"Accept": "application/json"}
-      });
-      if (!response.ok) {
-        throw new Error("request failed " + response.statusText);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Fetch error:', error);
-      throw error;
-    }
-  }
-
   public async getMunicipalityData(electionId: string, municipalityName: string): Promise<ConstituencyPartyVotes[]> {
     const url = `${API_BASE_URL}/elections/${electionId}/municipalities/${municipalityName}`;
 
@@ -82,39 +64,4 @@ export class ElectionService {
     }
   }
 
-  public async getConstituencyData(electionId: string, constituencyName: string): Promise<ConstituencyPartyVotes[]> {
-    const url = `${API_BASE_URL}/elections/${electionId}/${constituencyName}`;
-
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {"Accept": "application/json"}
-      });
-      if (!response.ok) {
-        throw new Error("request failed " + response.statusText);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Fetch error:', error);
-      throw error;
-    }
-  }
-
-  public static async getConstituencyVotesPercentage(electionId: string, constituencyName: string): Promise<number> {
-    const url = `${API_BASE_URL}/elections/${electionId}/${constituencyName}/votes-percentage`;
-
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {"Accept": "application/json"}
-      });
-      if (!response.ok) {
-        throw new Error(`request failed ${response.statusText}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Fetch error:', error);
-      throw error;
-    }
-  }
 }
