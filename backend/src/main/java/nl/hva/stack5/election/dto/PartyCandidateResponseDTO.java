663e91c68ddb1f6b788ebe565affba04c9ff16c5
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-27 14:03:56 +0100

    feat(DTO): create partyCandidateResponse dto with variables and constructors

---
+++ backend/src/main/java/nl/hva/stack5/election/dto/PartyCandidateResponseDTO.java
@@ -0,0 +1,24 @@
+package nl.hva.stack5.election.dto;
+
+/**
+ * DTO for candidate information within a party detail view.
+ * Includes percentage of votes within the party.
+ *
+ * @author Matisse Ben Addi
+ * @version 1.0
+ */
+public class PartyCandidateResponseDTO {
+    private long id;
+    private String fullName;
+    private long votes;
+    private double partyPercentage;
+
+    public PartyCandidateResponseDTO() {}
+
+    public PartyCandidateResponseDTO(long id, String fullName, long votes, double partyPercentage) {
+        this.id = id;
+        this.fullName = fullName;
+        this.votes = votes;
+        this.partyPercentage = partyPercentage;
+    }
+}
