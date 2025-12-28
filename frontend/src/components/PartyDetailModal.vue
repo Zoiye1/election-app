8115249f1b0c9c1d92b9724e0bba9dc53bfbd963
Matisse Ben Addi <matissegreetz@gmail.com>
2025-12-28 10:39:52 +0100

    feat(Component): add props, emit for closing modal and computed partyLogoUrl getter

---
+++ frontend/src/components/PartyDetailModal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import type { PartyDetail } from '@/interfaces/IElectionData.ts'
+import { computed } from 'vue'
+import { getPartyLogoUrl } from '@/utils/partyLogos.ts'
+
+const props = defineProps<{
+  party: PartyDetail
+  ranking: number
+}>()
+
+const emit = defineEmits<{
+  close: []
+}>()
+
+const partyLogoUrl = computed(() => {
+  return getPartyLogoUrl(props.party.partyName)
+})
+
+</script>
